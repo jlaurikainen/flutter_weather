693cd8a4d564539549f893effdc44a2625673147
Juha Laurikainen <jmlaurikainen@gmail.comn>
2025-06-27 03:18:01 +0300

    Updated NDK version required by location

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.weather"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.2.12479018"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
